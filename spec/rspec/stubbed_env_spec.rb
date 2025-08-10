37186b334d4bccdc8fe6540439a9c8b2e3ae5479
Peter H. Boling <peter.boling@gmail.com>
2025-08-10 15:33:36 -0600

    ✅ Add test

--- spec/rspec/stubbed_env_spec.rb
+++ spec/rspec/stubbed_env_spec.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe(RSpec::StubbedEnv) do
-  it "has a version number" do
+  it "has a (deprecated) version number" do
     expect(RSpec::StubbedEnv::VERSION).not_to(be_nil)
+  end
+
+  it "has a version number" do
+    expect(RSpec::StubbedEnv::Version::VERSION).not_to(be_nil)
   end
 end
